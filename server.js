d8f816df25f6f4668117cdede4629b756b42f2b6
terrence <380135745@qq.com>
2016-03-03 15:59:39 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+var debug = require('debug');
+var app = require('./app');
+
+app.set('port', process.env.PORT || 8100);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('微信公众号服务启动端口: ' + server.address().port);
+    debug('微信公众号服务启动端口: ' + server.address().port);
+});
