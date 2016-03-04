3836e0894c254face61738296af63c86af5b0bf3
terrence <380135745@qq.com>
2016-03-04 11:10:20 +0800

    增加微信验证接入，修改端口为80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var debug = require('debug');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 443);
+app.set('port', process.env.PORT || 80);
 
 var server = app.listen(app.get('port'), function() {
     console.log('微信公众号服务启动端口: ' + server.address().port);
