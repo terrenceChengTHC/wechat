b2e2bbc5f401303de350ddbf8f2ab738da339921
terrence <380135745@qq.com>
2016-03-06 21:16:51 +0800

    完善ejs架构

---
+++ routes/access.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var url = require("url");
+var crypto = require("crypto");
+var router = express.Router();
+
+/* wechat invalidate access */
+router.get('/', function(req, res, next) {
+    var query = url.parse(req.url,true).query;
+    var signature = query.signature;
+    var echostr = query.echostr;
+    var timestamp = query['timestamp'];
+    var nonce = query.nonce;
+    var oriArray = new Array();
+    oriArray[0] = nonce;
+    oriArray[1] = timestamp;
+    oriArray[2] = "8218ac8d070a354";//wechat api setting token
+    oriArray.sort();
+    var original = oriArray.join('');
+    console.log("Original str : " + original);
+    console.log("Signature : " + signature );
+    var scyptoString = sha1(original);
+    if(signature == scyptoString){
+        res.end(echostr);
+        console.log("Confirm and send echo back");
+    }else {
+        res.end("false");
+        console.log("Failed!");
+    }
+});
+
+function sha1(str){
+    var md5sum = crypto.createHash("sha1");
+    md5sum.update(str);
+    str = md5sum.digest("hex");
+    return str;
+}
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,9 @@ var url = require("url");
 var crypto = require("crypto");
 var router = express.Router();
 
-/* ÑéÖ¤½ÓÈëÎ¢ĞÅ¹«ÖÚºÅ */
-router.get('/', function(req, res, next) {
-  console.log('index');
-  var query = url.parse(req.url,true).query;
-  var signature = query.signature;
-  var echostr = query.echostr;
-  var timestamp = query['timestamp'];
-  var nonce = query.nonce;
-  var oriArray = new Array();
-  oriArray[0] = nonce;
-  oriArray[1] = timestamp;
-  oriArray[2] = "8218ac8d070a354";//ÕâÀïÊÇÄãÔÚÎ¢ĞÅ¿ª·¢ÕßÖĞĞÄÒ³ÃæÀïÌîµÄtoken
-  oriArray.sort();
-  var original = oriArray.join('');
-  console.log("Original str : " + original);
-  console.log("Signature : " + signature );
-  var scyptoString = sha1(original);
-  if(signature == scyptoString){
-    res.end(echostr);
-    console.log("Confirm and send echo back");
-  }else {
-    res.end("false");
-    console.log("Failed!");
-  }
+/* wechat invalidate access */
+router.get('/', function (req, res, next) {
+    res.render('index',{title:'æˆ‘çš„æ·˜ç±³u å•Š'})
 });
-
-function sha1(str){
-  var md5sum = crypto.createHash("sha1");
-  md5sum.update(str);
-  str = md5sum.digest("hex");
-  return str;
-}
 
 module.exports = router;
